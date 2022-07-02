7684d8c1ef6abee3018cf9b700a3ba34e62c6aee
Ganesh Kumar S M <ksganesh18@gmail.com>
2022-07-02 23:47:52 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    from munch import DefaultMunch
-# DefaultMunch is used for recursive lookup.
-
-    my_object = {'a': {'b': {'c': 'd'}}}
-    obj = DefaultMunch.fromDict(my_object)
-    print(obj.a.b.c)
-
